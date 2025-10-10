29d895f3d126d87cdf222afc468028bc3e737a47
Trevor <valentinosolischang@gmail.com>
2025-10-10 17:30:03 -0500

    Add menu items for navigation

---
+++ src/consts/menu-item.ts
@@ -0,0 +1,11 @@
+import type { MenuItem } from "@/types/MenuItem";
+
+const menuItems: MenuItem[] = [
+  { name: "Beneficios", href: "#beneficios" },
+  { name: "Ejercicios", href: "#ejercicios" },
+  { name: "Planes", href: "#planes" },
+  { name: "Comentarios", href: "#comentarios" },
+  { name: "Psicólogos", href: "#psicologos" },
+];
+
+export default menuItems;
