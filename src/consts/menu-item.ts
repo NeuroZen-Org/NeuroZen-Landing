15a2b9a0409de654a3107a03ad8b6736e1844069
Trevor <valentinosolischang@gmail.com>
2025-12-03 11:03:24 -0500

    feat: Add "About Us" menu item and translations for navigation

--- src/consts/menu-item.ts
+++ src/consts/menu-item.ts
@@ -3,6 +3,7 @@ import type { MenuItem } from "@/types/MenuItem";
 const menuItems: MenuItem[] = [
   { name: "nav.benefits", href: "#beneficios" },
   { name: "nav.exercises", href: "#ejercicios" },
+  { name: "nav.aboutus", href: "#nosotros" },
   { name: "nav.plans", href: "#planes" },
   { name: "nav.comments", href: "#comentarios" },
   { name: "nav.psychologists", href: "#psicologos" },
